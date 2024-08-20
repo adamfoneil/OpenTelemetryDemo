17e34627c8f0dfc8c1869db36bfd034aa83a77b1
Adam O'Neil <adamosoftware@gmail.com>
2024-08-20 07:36:56 -0400

    trying stuff, but not working

--- OpenTelemetryDemo/Program.cs
+++ OpenTelemetryDemo/Program.cs
@@ -1,18 +1,29 @@
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using OpenTelemetryDemo.Components;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
 
-builder.Services.AddOpenTelemetry().WithTracing(trace =>
-	trace.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("BlazorApp"))
-	.AddAspNetCoreInstrumentation()
-	.AddHttpClientInstrumentation()
-	.AddEntityFrameworkCoreInstrumentation()
-	.AddOtlpExporter(options => options.Endpoint = new Uri("http://jaeger:4317")));
+builder.Services.AddOpenTelemetry().ConfigureResource(resource =>
+	resource.AddService("BlazorApp").AddAttributes(new Dictionary<string, object>()
+	{
+		["service.version"] = Assembly.GetExecutingAssembly().GetName().Version!.ToString(),
+		["environment"] = builder.Environment.EnvironmentName
+	})).WithTracing(trace =>
+		trace
+			.AddAspNetCoreInstrumentation()
+			.AddHttpClientInstrumentation()
+			.AddEntityFrameworkCoreInstrumentation()
+			.AddConsoleExporter()
+			.AddOtlpExporter(options =>
+			{
+				options.Endpoint = new Uri("http://jaeger:4317");
+				options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;				
+			}));
 
 var app = builder.Build();
 
